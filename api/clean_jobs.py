47b07dd73f3bcda48040469844fd5a812ab53ca3
ElsabbahInstitute <ahmad.m.mustapha@hotmail.com>
2024-02-28 13:34:13 +0200

    Clean jobs by hours

--- api/clean_jobs.py
+++ api/clean_jobs.py
@@ -2,13 +2,19 @@
 import os, time
 from app import TEMP_DIR_NAME
 from codebase.utils import remove_directory
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A script to remove generation jobs.')
+    parser.add_argument('--hours', type=float, help='The age of jobs that should be deleted in hours')
+
+    args = parser.parse_args()
+
     for job_directory in os.listdir(TEMP_DIR_NAME):
         job_path = os.path.join(TEMP_DIR_NAME, job_directory)
         creation_time = os.path.getmtime(job_path)
-        time_delta = (time.time() - creation_time)/(3600*24)
-        if time_delta >= 1:
+        time_delta = (time.time() - creation_time)/(3600)
+        if time_delta >= args.hours:
             remove_directory(job_path)
             print(f"Removed {job_directory}")
 
